3470703e1e82cda51b640f63470114b02e599b30
groppedev <massimo.groppelli@gmail.com>
2020-04-21 00:22:53 +0200

    Added compilation command

--- execute.sh
+++ execute.sh
@@ -1,1 +1,2 @@
+javac src/it/groppedev/jit/Test.java
 java -client -Xbatch -XX:+PrintCompilation -XX:CompileThreshold=5 -XX:-TieredCompilation -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly -XX:+DebugNonSafepoints -XX:CompileCommand=quiet -XX:CompileCommand=exclude,java/*,* -XX:CompileCommand=exclude,sdk/*,* -XX:CompileCommand=exclude,sun/*,* -cp src it.groppedev.jit.Test
