3ad4fe1f617cf26cc8127751243915d4deed3a7b
groppedev <massimo.groppelli@gmail.com>
2020-04-20 23:28:53 +0200

    fixed classpath

--- execute.sh
+++ execute.sh
@@ -1,1 +1,1 @@
-java -client -Xbatch -XX:+PrintCompilation -XX:CompileThreshold=5 -XX:-TieredCompilation -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly -XX:+DebugNonSafepoints -XX:CompileCommandFile=hotspot_compiler -cp src/* it.groppedev.jit.Test
+java -client -Xbatch -XX:+PrintCompilation -XX:CompileThreshold=5 -XX:-TieredCompilation -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly -XX:+DebugNonSafepoints -XX:CompileCommandFile=hotspot_compiler -cp src it.groppedev.jit.Test
